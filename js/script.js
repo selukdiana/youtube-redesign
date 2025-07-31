ea551d52a52034ea9ae5e8d9ab60caef8c08c5fa
Dziyana.Sialiuk <dianaselyuk02@mail.ru>
2025-07-31 12:07:15 +0300

    feat: finish channel page

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// document.querySelector(".burger").addEventListener("click", () => {
-//   const sidebar = document.querySelector(".sidebar");
-//   sidebar.classList.toggle("hidden");
-// });
+document.querySelector(".burger").addEventListener("click", () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("hidden");
+});
 
 const currentUrl = window.location.href;
 console.log(currentUrl);
 
-const fetchLayout = async () => {
-  const sidebar = await fetch("../sidebar.html");
-  const sidebarHTML = await sidebar.text();
-  document.querySelector(".sidebar").outerHTML = sidebarHTML;
-  const header = await fetch("../header.html");
-  const headerHTML = await header.text();
-  document.querySelector(".header").outerHTML = headerHTML;
-  document.querySelector(".burger").addEventListener("click", () => {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("hidden");
-  });
-};
+// const fetchLayout = async () => {
+//   const sidebar = await fetch("../sidebar.html");
+//   const sidebarHTML = await sidebar.text();
+//   document.querySelector(".sidebar").outerHTML = sidebarHTML;
+//   const header = await fetch("../header.html");
+//   const headerHTML = await header.text();
+//   document.querySelector(".header").outerHTML = headerHTML;
+//   document.querySelector(".burger").addEventListener("click", () => {
+//     const sidebar = document.querySelector(".sidebar");
+//     sidebar.classList.toggle("hidden");
+//   });
+// };
 
-fetchLayout();
+// fetchLayout();
